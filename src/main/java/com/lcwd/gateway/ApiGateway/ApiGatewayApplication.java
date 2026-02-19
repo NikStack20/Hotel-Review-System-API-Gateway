7b40e85a5e9104d1dc45aa778837c6ea89c8447d
unknown <chauhannikhil20062005@gmail.com>
2026-02-19 19:43:46 +0530

    Configured Api-Gateway for the microservices and also updated the Maven Runtime.

--- src/main/java/com/lcwd/gateway/ApiGateway/ApiGatewayApplication.java
+++ src/main/java/com/lcwd/gateway/ApiGateway/ApiGatewayApplication.java
@@ -1,6 +1,6 @@
 package com.lcwd.gateway.ApiGateway;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
