7cd3ce014b7efa95f10d7ab975a68dc620e7dd61
unknown <codingnik20@gmail.com>
2026-04-08 15:30:29 +0530

    Added okta OAuth2 to the Api-Gateway.

--- src/main/java/com/lcwd/gateway/ApiGateway/ApiGatewayApplication.java
+++ src/main/java/com/lcwd/gateway/ApiGateway/ApiGatewayApplication.java
@@ -1,8 +1,7 @@
 package com.lcwd.gateway.ApiGateway;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 
